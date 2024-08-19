f7b6dfe3829440064404da6cc92b6f43e82b6726
Your Name <anakillmesho@gmail.com>
2024-08-20 00:28:08 +0300

    lf

--- myenv.js
+++ myenv.js
@@ -1,4 +1,4 @@
 
-  export  const  API_URL="http://localhost:5000"//https://badroobot-badroobot.koyeb.app"
+  export  const  API_URL="https://badroobot-badroobot.koyeb.app"//https://badroobot-badroobot.koyeb.app"
   export const BEARER_TOKEN="AZZA_Ai_abosherif_mahmoud_sherif_QpdADuruI29cePlNjMrckTKELQlADuFDGWbYnZpAZZA"
   export const APPNAME='Crochet Creations'
